ff93dc63963a454e3aa3fbfac5622f4148721d28
Jacob Peyron <jacob.peyron@gmail.com>
2024-02-10 13:29:44 +0100

    implemented schedule

--- gencal.py
+++ gencal.py
@@ -1,7 +1,9 @@
 """ gencal
 Program to generate a rolling calendar, based on a schedule
 """
+import csv
 from datetime import date, timedelta
+from typing import Optional
 
 NOP_E = "x"
 DAG_E = "Dag"
@@ -12,10 +14,11 @@ KP_E = "KP"
 
 # Mimic Google Calendar CSV format
 IS_EMPTY = "empty"
+SUB = "Subject"
 START_DATE = "Start Date"
 END_DATE = "End Date"
 START_TIME = "Start Time"
-END_TIME = "Start Time"
+END_TIME = "End Time"
 # DESC = "Description"
 # ALL_DAY = "All Day Event"
 
@@ -25,34 +28,39 @@ ADD_DAYS = "_adddays"
 EVENTS = {
     NOP_E: {IS_EMPTY: True},
     DAG_E: {
-        IS_EMPTY: False,
+        SUB: DAG_E,
         START_TIME: "07:10",
         END_TIME:   "15:24",
         ADD_DAYS: 0,
+        IS_EMPTY: False,
     },
     HEL_E: {
-        IS_EMPTY: False,
+        SUB: HEL_E,
         START_TIME: "07:10",
         END_TIME:   "19:50",
         ADD_DAYS: 0,
+        IS_EMPTY: False,
     },
     KVALL_E: {
-        IS_EMPTY: False,
+        SUB: KVALL_E,
         START_TIME: "12:00",
         END_TIME:   "19:50",
         ADD_DAYS: 0,
+        IS_EMPTY: False,
     },
     NATT_E: {
-        IS_EMPTY: False,
+        SUB: NATT_E,
         START_TIME: "19:40",
         END_TIME:   "07:20",
         ADD_DAYS: 1,
+        IS_EMPTY: False,
     },
     KP_E: {
-        IS_EMPTY: False,
+        SUB: KP_E,
         START_TIME: "08:30",
         END_TIME:   "15:28",
         ADD_DAYS: 0,
+        IS_EMPTY: False,
     }
 }
 
@@ -63,7 +71,11 @@ SCHEDULE = [
     NOP_E,  KVALL_E, KVALL_E, KVALL_E, KP_E,    NOP_E,  NOP_E,
     HEL_E,  DAG_E,   DAG_E,  DAG_E,     NOP_E,  NOP_E,  NOP_E,
 ]
-S_START = date(2024, 2, 12)
+# When is the first element in SCHEDULE, when n = 0
+SCHED_START = date(2024, 2, 12)
+
+OUT_START = SCHED_START  # What date to output first
+OUT_COUNT = 31*7-16 # How many days to output 
 
 def event(n: int) -> dict[str, any]:
     mod = n % len(SCHEDULE)
@@ -71,12 +83,35 @@ def event(n: int) -> dict[str, any]:
     return EVENTS[s]
 
 def start_date(n: int) -> date:
-    return S_START + timedelta(days=n)
+    return SCHED_START + timedelta(days=n)
+
+def csv_row(n, d) -> Optional[list]:
+    e = event(n)
+    if e[IS_EMPTY]:
+        return None
+
+    subject = e[SUB]
+
+    days_to_add = e[ADD_DAYS]
+    start_d = start_date(n)
+    end_d = start_d + timedelta(days=days_to_add)
+
+    start_t = e[START_TIME]
+    end_t = e[END_TIME]
+    return [subject, start_d, end_d, start_t, end_t]
+
 
 def main():
-    exp = [EVENTS[s] for s in SCHEDULE]
-    res = [event(n) for n in range(len(SCHEDULE))]
-    print(exp == res)
+    d = OUT_START
+
+    with open("sched.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([SUB, START_DATE, END_DATE, START_TIME, END_TIME])
+        for n in range(OUT_COUNT):
+            row = csv_row(n, d)
+            if row is not None:
+                writer.writerow(row)
+            d = d + timedelta(days=1)
 
 if __name__ == "__main__":
     main()
